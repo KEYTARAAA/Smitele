9c1666b19a8ded51631f2fcacead37d574216bde
KITARA STEWART <18241476@studentmail.ul.ie>
2024-05-29 13:53:35 +0100

    Add layout to home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,15 @@
+import "../styles/HomePage.css";
 function HomePage() {
   return (
     <>
-      <a href="/god">
-        <button>God of the Day</button>
-      </a>
-      <a href="/ability">
-        <button>Ability of the Day</button>
-      </a>
+      <div className="layout">
+        <a href="/god">
+          <button>God of the Day</button>
+        </a>
+        <a href="/ability">
+          <button>Ability of the Day</button>
+        </a>
+      </div>
     </>
   );
 }
